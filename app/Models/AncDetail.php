867ad9782cd66a7d6e3746d687cdadc47e4e29d2
Mamad Ahmad <mamadahmadfnc@gmail.com>
2026-04-29 18:30:18 +0700

    membuat model

---
+++ app/Models/AncDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AncDetail extends Model
+{
+    use HasFactory;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,32 +12,23 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'role_id'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function kunjungan()
+    {
+        return $this->hasMany(Kunjungan::class);
+    }
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
